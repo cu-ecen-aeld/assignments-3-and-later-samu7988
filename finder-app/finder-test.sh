a01878a1b39a6eb870ea49d457a9baea9d308e4c
samu7988 <samu7988@colorado.edu>
2022-02-05 20:54:06 -0700

    Adding modified  finder-test.sh

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -10,8 +10,9 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
-
+username=$(cat /etc/finder-app/conf/username.txt)
+WRITER_PATH=$(which writer)
+FINDER_PATH=$(which finder.sh) 
 if [ $# -lt 2 ]
 then
 	echo "Using default value ${WRITESTR} for string to write"
@@ -50,11 +51,13 @@ fi
 for i in $( seq 1 $NUMFILES)
 do
 	#./writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	#writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	"$WRITER_PATH" "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
-
+#OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$("FINDER_PATH" "$WRITEDIR" "$WRITESTR")
+echo ${OUTPUTSTRING} > /tmp/assignment-4-result.txt
 set +e
 echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
 if [ $? -eq 0 ]; then
