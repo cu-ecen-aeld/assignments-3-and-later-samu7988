3bc70d36618cdc34fe3341f63604f133fd114940
samu7988 <samu7988@colorado.edu>
2022-03-03 21:45:27 -0700

    FEAT(circular_buffer):Implemented circular buffer add entry

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -16,6 +16,38 @@
 
 #include "aesd-circular-buffer.h"
 
+struct aesd_buffer_entry
+{
+	/**
+	 * A location where the buffer contents in buffptr are stored
+	 */
+	const char *buffptr;
+	/**
+	 * Number of bytes stored in buffptr
+	 */
+	size_t size;
+};
+
+struct aesd_circular_buffer
+{
+	/**
+	 * An array of pointers to memory allocated for the most recent write operations
+	 */
+	struct aesd_buffer_entry  entry[AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED];
+	/**
+	 * The current location in the entry structure where the next write should
+	 * be stored.
+	 */
+	uint8_t in_offs;
+	/**
+	 * The first location in the entry structure to read from
+	 */
+	uint8_t out_offs;
+	/**
+	 * set to true when the buffer entry structure is full
+	 */
+	bool full;
+};
 /**
  * @param buffer the buffer to search for corresponding offset.  Any necessary locking must be performed by caller.
  * @param char_offset the position to search for in the buffer list, describing the zero referenced
@@ -47,6 +79,53 @@ void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const s
     /**
     * TODO: implement per description 
     */
+    if(buffer == NULL)
+    {
+    	printf("aesd_circular_buffer_add_entry(): buffer is NULL\nr");
+    }
+    
+    if(add_entry == NULL)
+    {
+    	printf("aesd_circular_buffer_add_entry(): add_entry is NULL\n\r");
+    }
+    
+    //Check if buffer is full
+    if(buffer->full == 1)
+    {
+    	//Overwrite the oldest entry
+    	memcpy(&(buffer->entry[buffer->in_offs]),add_entry);
+    	buffer->in_offs += 1;
+    	if(buffer->in_offs == AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED)
+    	{
+    	  buffer->in_offs = 0;
+    	}
+    	
+    	//advance out_offs by 1
+    	buffer->out_offs += 1;
+    	if(buffer->out_offs == AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED)
+    	{
+    	  buffer->out_offs = 0;
+    	}
+    }
+    else
+    {
+    	memcpy(&(buffer->entry[buffer->in_offs]),add_entry);
+    	buffer->in_offs += 1;
+    	
+    	
+    	//if in_offs reaches end, wrap around
+    	if(buffer->in_offs == AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED)
+    	{
+    	  buffer->in_offs = 0;
+    	}
+    	
+    	//if in_offs == out_offs
+    	if(buffer->in_offs == buffer->out_offs)
+    	{
+    	  buffer->full = 1;
+    	}
+    	    	
+    }
 }
 
 /**
